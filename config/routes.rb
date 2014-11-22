a8fd635f9c713cfcbb81c3ab3520fc8bdf71b610
nbhartiya <neeharika@alum.mit.edu>
2014-11-22 11:54:49 -0800

    added homepage view and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Messageboard::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'controller_name#action_name'
+  root 'home#homepage'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
