48a17b8a66187b753195e1d49901e4395cc5a36b
nbhartiya <neeharika@alum.mit.edu>
2014-11-22 10:48:57 -0800

    create rails app and home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messageboard::Application.config.secret_key_base = 'a4338a4be332ba4ed134b7fe96a639ebaebdf2d50deb38787a7e13bae086a413a90fbd4f3cef91124100bd74544b63e0e44d71268785d190782f4447603f9560'
